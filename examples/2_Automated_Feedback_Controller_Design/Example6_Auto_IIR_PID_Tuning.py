2ac23ec76bfe80fd4370b28dc8171af4f412fdc7
shimodatakaki <shimodatakaki@gmail.com>
2017-12-30 18:00:17 +0900

    CLEAN myfbcd example6

--- examples/2_Automated_Feedback_Controller_Design/Example6_Auto_IIR_PID_Tuning.py
+++ examples/2_Automated_Feedback_Controller_Design/Example6_Auto_IIR_PID_Tuning.py
@@ -23,28 +23,73 @@ def optimize(fig, o, g, datapath=DATA):
     THETA_DPM2 = 30 / 180 * np.pi  # Second Phase Margin
     GDB_DGM = 5  # Gain Margin in (dB)
 
-    NSTBITER = 3
+    NSTBITER = 1
 
-    TAUD = 2 * TS  # Pseudo Differential Cut-off for D Control
-    NOFIR = 0  # Only PID
-    NOPID = "pid"
+    if True:
+        # PID Triple Poleassignment 400 Hz
+        rhon = np.array([0.000101889491467777,
+                         0.170717481532357,
+                         107.264957164280])
+        rhod = np.array([9.94718394324346 * 10 ** (-5), 1])
+    elif False:
+        # PID 400 Hz  + 5400 Hz Notch
+        rhon = np.array([0.000178306610068610,
+                         0.846420925630186,
+                         368574.319724049,
+                         1077291892.56301,
+                         1183397129740.58])
+        rhod = np.array([5.68410511042483e-05,
+                         1.51578813420334,
+                         126083.578322355,
+                         2058536168.05419])
+    elif False:
+        rhon = np.array([0.000127361864334721,
+                         0.787836113815149,
+                         676560.592842885,
+                         2760708571.14940,
+                         853187331902783,
+                         1.78089599036966e+18,
+                         1.39697403340072e+21
+                         ])
+        rhod = np.array([7.95774715459477e-05,
+                         4.43955832426420,
+                         489464.466739361,
+                         13540677611.4662,
+                         630318001739791,
+                         6.66807430879530e+18
+                         ])
+    else:
+        # PID 300 Hz + Phase Compensator + Notch
+        rhon = np.array([7.64171186008328e-05,
+                         6.23379530477791,
+                         175952.995822072,
+                         12564320886.3040,
+                         15618222242330.9,
+                         7.31297798873055e+15
+                         ])
+        rhod = np.array([0.000132629119243246,
+                         5.51210670192588,
+                         337065.291819523,
+                         9095778966.23193,
+                         51352787209705.0
+                         ])
 
-    f = 150
+    f = 300
     _f = [0]
     _c = [None]
     rho = None
-    rhon = []
-    rhod = []
     rho_best = rho
-    R = 1.5
+    R = 1.1
     LAMBDA = (1 + R) / R / 2
-    tol = 15
+    tol = 10
     while tol > 0:
-        F_DGC = 2 * np.pi * f  # Desired Cross-over Frequency (rad/s)
         print("Try: ", f, " Hz")
+
+        fbc = IIRControllerDesign(o, g, ts=TS, rhon=rhon, rhod=rhod)
+        F_DGC = 2 * np.pi * f  # Desired Cross-over Frequency (rad/s)
+        fbc.specification(F_DGC, THETA_DPM, GDB_DGM, theta_dpm2=THETA_DPM2)  # set constraints
+
         for i in range(NSTBITER):
-            fbc = IIRControllerDesign(o, g, ts=TS, rhon=rhon, rhod=rhod)
-            fbc.specification(F_DGC, THETA_DPM, GDB_DGM, theta_dpm2=THETA_DPM2)  # set constraints
             fbc.nominalcond(db=-60)  # append nominal performance condition
             fbc.stabilitycond()  # append stability condition
             fbc.gainpositivecond()  # append gain constraints
@@ -58,10 +103,13 @@ def optimize(fig, o, g, datapath=DATA):
 
             rhon = fbc.rhon
             rhod = fbc.rhod
+
             fbc.controller()
             fbc.openloop()
-            taud = fbc.rho[-1]
-            pid = btaud2pid(fbc.rho[:3], taud)
+            fbc.reset()
+
+            taud = fbc.rhod[0] / fbc.rhod[1]
+            pid = btaud2pid([x / fbc.rhod[1] for x in fbc.rhon], taud)
             print("rho:", fbc.rho)
             print("PIDs:", pid)
             print("TAU:", taud)
@@ -77,10 +125,6 @@ def optimize(fig, o, g, datapath=DATA):
                 _c.append(fbc)
                 f *= R
                 break
-        # if i == NSTBITER-1:
-        #     break
-        if f > 500:
-            break
 
     for e in range(11, 0, -1):
         print((11 - e) * ' ' + e * '*')
@@ -92,13 +136,13 @@ def optimize(fig, o, g, datapath=DATA):
     i_max = [i for i, f in enumerate(_f) if f == max(_f)][0]
     print("Best nominal frequency:", _f[i_max], " Hz")
     fbc = _c[i_max]
-    taud = fbc.rho[-1]
-    pid = btaud2pid(fbc.rho[:3], taud)
+    taud = fbc.rhod[0] / fbc.rhod[1]
+    pid = btaud2pid([x / fbc.rhod[1] for x in fbc.rhon], taud)
     print("rho:", fbc.rho)
     print("PIDs:", pid)
     print("TAU:", taud)
     mycsv.save([*pid, taud], save_name=DATA + "/rho" + str(fig) + ".csv",
-               header=("P,I,D, FIR(1+n) for n in range(" + str(NOFIR) + ")", "taud (s):" + str(TAUD),
+               header=("taud (s):" + str(taud),
                        "FIR sampling (s):" + str(TS)))
 
     return fig, fbc
