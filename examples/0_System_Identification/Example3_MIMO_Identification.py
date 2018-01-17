cc791bd12ebd89e5c9add85156dcfd1fad6a3b4c
shimodatakaki <shimodatakaki@gmail.com>
2018-01-17 09:17:56 +0900

    FIX myfdi exmaple 3

--- examples/0_System_Identification/Example3_MIMO_Identification.py
+++ examples/0_System_Identification/Example3_MIMO_Identification.py
@@ -10,6 +10,7 @@ R = 1.02  # ratio of frequency evolution
 F_RANGE = (10, 200)
 NOI = 10  # Number of iteration
 
+
 def sys_id():
     path = get_path(*F_RANGE, "data_1")
     sys_id_11 = SystemIdentification(n_num=3, n_den=4, path=path, transient=3)
@@ -28,7 +29,7 @@ def sys_id():
     print("nin, nout: ", mimo_id.nin, mimo_id.nout)
     print("theta_0:\n", mimo_id.theta_0)
     print("nof: ", mimo_id.nof)
-    print("n_den, n_num: ",mimo_id.n_den, mimo_id.n_num)
+    print("n_den, n_num: ", mimo_id.n_den, mimo_id.n_num)
     print("nop: ", mimo_id.nop)
 
     theta_mle = mimo_id.nonlinear_least_squares()
@@ -36,6 +37,7 @@ def sys_id():
     print(theta_mle)
 
     # Transfer Function of Simulated Transfer Function
+
     s = symbols('s')
     J = 0.01
     oz = 100 * 2 * np.pi
@@ -57,20 +59,23 @@ def sys_id():
     fig = 0
     for i, theta in enumerate(mimo_id.get_each(theta_mle)):
         print(theta)
-        save_theta_to_csv(mimo_id.n_den, theta, 'data/mimo_theta_mle_'+str(i+1)+'.csv')
+        save_theta_to_csv(mimo_id.n_den, theta, 'data/mimo_theta_mle_' + str(i + 1) + '.csv')
         G_mle = theta_to_tf(mimo_id.n_den, theta)
-        print("MLE", str(i),  G_mle)
+        print("MLE", str(i), G_mle)
         fig += 1
-        myplot.bode(G[i], fig, w=o_lines)
-        myplot.bode(G_mle, fig, w=o_lines,
-                    save_name=DATA_PATH + str(i) + "_MIMO_System_Identification",
-                leg=("P_true"+str(i), "P_mle"+str(i)))
+        myplot.bode(fig, G[i], w=o_lines)
+        myplot.bode(fig, G_mle, w=o_lines)
+        myplot.save(fig, save_name=DATA_PATH + str(i) + "_MIMO_System_Identification",
+                    leg=("P_true" + str(i), "P_mle" + str(i)))
 
 
 def main():
     sys_id()
 
+
 if __name__ == "__main__":
     from sympy import *
 
     main()
+
+    myplot.show()
